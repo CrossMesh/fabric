eecdb912cca98f7813f20a2352b7aa6ca799c5ce
Chad Mai <Sunmxt@126.com>
2019-12-26 22:41:44 +0800

    feat: abiter.

---
+++ pkg/arbiter/arbiter.go
@@ -0,0 +1,64 @@
+package arbiter
+
+import (
+	"os"
+	"os/signal"
+	"sync/atomic"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type Arbiter struct {
+	runningCount uint32
+	running      bool
+	sigFibreExit chan struct{}
+	log          *log.Entry
+}
+
+func New(log *log.Entry) *Arbiter {
+	return &Arbiter{
+		sigFibreExit: make(chan struct{}, 10),
+		log:          log,
+	}
+}
+
+func (a *Arbiter) Go(proc func()) {
+	go func() {
+		atomic.AddUint32(&a.runningCount, 1)
+		defer func() {
+			a.sigFibreExit <- struct{}{}
+		}()
+		proc()
+	}()
+}
+
+func (a *Arbiter) ShouldRun() bool { return a.running }
+
+func (a *Arbiter) Arbit() error {
+	sig := make(chan os.Signal)
+	signal.Notify(sig, os.Interrupt, os.Kill)
+
+Arbiting:
+	for {
+		select {
+		case s := <-sig:
+			if (s == os.Interrupt || s == os.Kill) && a.running {
+				a.running = false
+				if a.log != nil {
+					a.log.Info("shutting down...")
+				}
+			}
+
+		case <-a.sigFibreExit:
+			a.runningCount--
+			if a.runningCount < 1 {
+				break Arbiting
+			}
+		}
+	}
+
+	if a.log != nil {
+		a.log.Info("exiting...")
+	}
+	return nil
+}
