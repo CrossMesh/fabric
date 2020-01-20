1783803480a771275ec2aa6ca9e9323feee14164
Chad Mai <Sunmxt@126.com>
2020-01-21 00:22:48 +0800

    feat(gossip): state apply transaction.

--- pkg/gossip/gossip.go
+++ pkg/gossip/gossip.go
@@ -13,89 +13,141 @@ const (
 	DEAD      = 2
 )
 
-type Peer interface {
+type MembershipPeer interface {
+	GossiperStub() *Peer
+
 	State() (int, uint32, time.Time)
-	SetState(int, uint32, time.Time)
 	Region() string
+	IsSelf() bool
+	Valid() bool
+}
+
+type PeerReleaseTx struct {
+	p *Peer
+
+	stateUpdated  bool
+	regionUpdated bool
+
+	state        int
+	stateVersion uint32
+	region       string
+}
+
+// Region update region.
+func (t *PeerReleaseTx) Region(region string) {
+	if region == t.p.region {
+		return
+	}
+	t.regionUpdated, t.region = true, region
+}
+
+// State applies state read from remote peers.
+func (t *PeerReleaseTx) State(state int, stateVersion uint32) {
+	if state > 2 {
+		return
+	}
+	p := t.p
+	// a dead peer cannot be back to live.
+	if p.state == DEAD {
+		return
+	}
+	if state != DEAD || stateVersion > p.stateVersion || // always accept new version.
+		(state == SUSPECTED && p.state != SUSPECTED) { // some peers suspect. so am I.
+		t.stateUpdated, t.stateVersion, t.state = true, stateVersion, state
+	}
+}
+
+// ClaimAlive should be called after an acknowledgement received from remote peer.
+func (t *PeerReleaseTx) ClaimAlive() {
+	p := t.p
+	// a dead peer cannot be back to live.
+	if p.state == DEAD {
+		return
+	}
+	// I learn that the suspection is wrong. Clarify it.
+	if p.state != ALIVE {
+		t.stateUpdated, t.stateVersion, t.state = true, p.stateVersion+1, ALIVE
+	}
+}
+
+// ClaimDead should be called when an acknowledgement cannot received from remote peer.
+func (t *PeerReleaseTx) ClaimDead() {
+	p := t.p
+	// a dead peer cannot be back to live.
+	if p.state == DEAD {
+		return
+	}
+	// I suspect the peer is dead.
+	if p.state != SUSPECTED {
+		t.stateUpdated, t.stateVersion, t.state = true, p.stateVersion, SUSPECTED
+	}
+}
+
+type Peer struct {
+	lock            sync.RWMutex
+	g               *Gossiper
+	stateVersion    uint32
+	state           int
+	region          string
+	lastStateUpdate time.Time
+}
+
+func NewPeer(g *Gossiper) *Peer {
+	return &Peer{g: g}
+}
+
+func (p *Peer) State() (int, uint32, time.Time) { return p.state, p.stateVersion, p.lastStateUpdate }
+func (p *Peer) Region() string                  { return p.region }
+func (p *Peer) IsSelf() bool                    { return false }
+func (p *Peer) Valid() bool                     { return true }
+func (p *Peer) GossiperStub() *Peer             { return p }
+
+func (p *Peer) Tx(commit func(*PeerReleaseTx) bool) bool {
+	p.lock.Lock()
+	defer p.lock.Unlock()
+
+	tx, shouldCommit := &PeerReleaseTx{p: p}, false
+	shouldCommit = commit(tx) && (tx.regionUpdated || tx.stateUpdated)
+	if !shouldCommit {
+		return false
+	}
+	if tx.regionUpdated && !tx.stateUpdated {
+		tx.stateUpdated, tx.stateVersion, tx.state = true, p.stateVersion+1, p.state
+	}
+	if tx.regionUpdated {
+		if p.g != nil {
+			p.g.refreshRegion(p.region, tx.region, p)
+		}
+		p.region = tx.region
+	}
+	if tx.stateUpdated {
+		p.state, p.stateVersion = tx.state, tx.stateVersion
+		p.lastStateUpdate = time.Now()
+	}
+
+	return true
 }
 
 type GossipContext struct {
 	gossiper *Gossiper
 	term     uint32
-	peers    []Peer
+	peers    []MembershipPeer
 
 	lock       sync.Mutex
-	discovered []Peer
+	discovered []MembershipPeer
 }
 
 func (c *GossipContext) NumOfPeers() int { return len(c.peers) }
 
-func (c *GossipContext) Peer(index int) Peer {
+func (c *GossipContext) Peer(index int) MembershipPeer {
 	if index > len(c.peers) {
 		return nil
 	}
 	return c.peers[index]
 }
 
-// Indicate applies state read from remote peers.
-func (c *GossipContext) Indicate(idx int, version uint32, state int) {
-	// ignore invalid indications
-	if idx > len(c.peers) || state > 2 {
-		return
-	}
-	peer := c.peers[idx]
-	localState, localVersion, _ := peer.State()
-	// a dead peer cannot be back to live.
-	if localState == DEAD {
-		return
-	}
-	// always accept new state
-	if state == DEAD || version > localVersion {
-		peer.SetState(state, version, time.Now())
-		return
-	}
-	// some peers suspect. so am I.
-	if state == SUSPECTED && localState != SUSPECTED {
-		peer.SetState(state, version, time.Now())
-	}
-}
-
-// ClaimAlive should be called after an acknowledgement received from remote peer.
-func (c *GossipContext) ClaimAlive(idx int) {
-	if idx > len(c.peers) {
-		return
-	}
-	peer := c.peers[idx]
-	localState, localVersion, _ := peer.State()
-	// a dead peer cannot be back to live.
-	if localState == DEAD {
-		return
-	}
-	// I learn that the suspection is wrong. Clarify it.
-	if localState != ALIVE {
-		peer.SetState(ALIVE, localVersion+1, time.Now())
-	}
-}
-
-// ClaimDead should be called when an acknowledgement cannot received from remote peer.
-func (c *GossipContext) ClaimDead(idx int) {
-	if idx > len(c.peers) {
-		return
-	}
-	peer := c.peers[idx]
-	localState, localVersion, _ := peer.State()
-	// a dead peer cannot be back to live.
-	if localState == DEAD {
-		return
-	}
-	// I suspect the peer is dead.
-	if localState != SUSPECTED {
-		peer.SetState(SUSPECTED, localVersion, time.Now())
-	}
-}
-
 // Discover add new discovered peer.
-func (c *GossipContext) Discover(peer Peer) {
+func (c *GossipContext) Discover(peer MembershipPeer) {
 	if peer == nil {
 		return
 	}
@@ -109,7 +161,13 @@ func (c *GossipContext) Term() uint32 { return c.term }
 type Gossiper struct {
 	lock     sync.RWMutex
 	term     uint32
-	byRegion map[string][]Peer
+	newSeeds []MembershipPeer
+
+	memberLock sync.RWMutex
+	byRegion   map[string]map[*Peer]MembershipPeer
+
+	onRemove func(MembershipPeer)
+	onAppend func(MembershipPeer)
 
 	MinRegionPeers int
 	SuspectTimeout time.Duration
@@ -117,38 +175,44 @@ type Gossiper struct {
 
 func NewGossiper() (g *Gossiper) {
 	g = &Gossiper{
-		byRegion:       make(map[string][]Peer),
+		byRegion:       make(map[string]map[*Peer]MembershipPeer),
 		MinRegionPeers: 1,
 		SuspectTimeout: time.Minute * 5,
 	}
 	return
 }
 
+func (g *Gossiper) applyNewSeeds() {
+	for _, seed := range g.newSeeds {
+		g.addToRegion(seed.Region(), seed)
+		seed.GossiperStub().g = g
+	}
+	g.newSeeds = nil
+}
+
 func (g *Gossiper) Do(brust int32, transport func(*GossipContext)) {
 	if brust < 1 {
 		brust = 1
 	}
+	g.lock.Lock()
+	defer g.lock.Unlock()
+
+	g.applyNewSeeds()
 
 	gctx := &GossipContext{
-		peers:    make([]Peer, 0, brust),
+		peers:    make([]MembershipPeer, 0, brust),
 		gossiper: g,
 	}
 	gctx.term = atomic.AddUint32(&g.term, 1)
-
-	g.lock.RLock()
-	finishTransport := false
-	// for recovering from transport() panic
-	defer func() {
-		if !finishTransport {
-			g.lock.RUnlock()
-		}
-	}()
 
 	// select random N peers
 	// posibility proof:
 	//	P = m/i * (1 - 1/(i+1)) * (1 - 1/(i+2)) * ... * (1 - 1/n) = m/i * i/(i+1) * (i+1)/(i+2) * ... * (n-1)/n = m/n
 	idx := int32(0)
-	g.visitPeer(func(region string, peer Peer) bool {
+	g.visitPeer(func(region string, peer MembershipPeer) bool {
+		if peer.IsSelf() || !peer.Valid() {
+			return true
+		}
 		if idx < brust {
 			gctx.peers = append(gctx.peers, peer)
 		} else if n := rand.Int31n(idx); n < brust {
@@ -159,59 +223,78 @@ func (g *Gossiper) Do(brust int32, transport func(*GossipContext)) {
 	})
 
 	transport(gctx)
-	finishTransport = true
+	onRemove, onAppend := g.onRemove, g.onAppend
 
-	g.lock.RUnlock()
-	g.lock.Lock()
-	defer g.lock.Lock()
 	// add discovered member.
 	if len(gctx.discovered) > 0 {
 		for _, peer := range gctx.discovered {
 			if peer != nil {
+				if onAppend != nil {
+					onAppend(peer)
+				}
 				g.addToRegion(peer.Region(), peer)
 			}
 		}
 	}
 
 	// clean dead peers.
-	for region, peers := range g.byRegion {
-		for idx, peer := range peers {
+	for _, peers := range g.byRegion {
+		for _, peer := range peers {
 			if state, _, _ := peer.State(); state != DEAD || len(peers) <= g.MinRegionPeers {
 				continue
 			}
 
 			// remove peer.
-			peers[idx] = peers[len(peers)-1]
-			peers = peers[:len(peers)-1]
-			g.byRegion[region] = peers
+			if onRemove != nil {
+				onRemove(peer)
+			}
+			delete(peers, peer.GossiperStub())
+			peer.GossiperStub().g = nil
 		}
 	}
-
 }
 
-func (g *Gossiper) addToRegion(region string, peer Peer) {
-	if peer == nil {
-		return
-	}
-	regionPeers, _ := g.byRegion[region]
+func (g *Gossiper) getRegionSet(region string) (regionPeers map[*Peer]MembershipPeer) {
+	regionPeers, _ = g.byRegion[region]
 	if regionPeers == nil {
-		regionPeers = make([]Peer, 0, 1)
+		regionPeers = make(map[*Peer]MembershipPeer)
+		g.byRegion[region] = regionPeers
 	}
-	regionPeers = append(regionPeers, peer)
-	g.byRegion[region] = regionPeers
 	return
 }
 
-func (g *Gossiper) Seed(peers ...Peer) {
+func (g *Gossiper) addToRegion(region string, peer MembershipPeer) {
+	if peer == nil {
+		return
+	}
+	g.memberLock.Lock()
+	defer g.memberLock.Unlock()
+
+	g.getRegionSet(region)[peer.GossiperStub()] = peer
+}
+
+func (g *Gossiper) refreshRegion(old, new string, peer MembershipPeer) {
+	g.memberLock.Lock()
+	defer g.memberLock.Unlock()
+
+	regionPeers := g.getRegionSet(old)
+	delete(regionPeers, peer.GossiperStub())
+	regionPeers[peer.GossiperStub()] = peer
+}
+
+func (g *Gossiper) Seed(peers ...MembershipPeer) {
 	g.lock.Lock()
 	defer g.lock.Unlock()
+
 	for _, peer := range peers {
-		peer.SetState(SUSPECTED, 0, time.Now())
-		g.addToRegion(peer.Region(), peer)
+		if peer == nil {
+			continue
+		}
+		g.newSeeds = append(g.newSeeds, peer)
 	}
 }
 
-func (g *Gossiper) visitPeer(visit func(string, Peer) bool) {
+func (g *Gossiper) visitPeer(visit func(string, MembershipPeer) bool) {
 	for region, peers := range g.byRegion {
 		for _, peer := range peers {
 			if !visit(region, peer) {
@@ -221,13 +304,14 @@ func (g *Gossiper) visitPeer(visit func(string, Peer) bool) {
 	}
 }
 
-func (g *Gossiper) VisitPeer(visit func(string, Peer) bool) {
+func (g *Gossiper) VisitPeer(visit func(string, MembershipPeer) bool) {
 	g.lock.RLock()
 	defer g.lock.RUnlock()
+
 	g.visitPeer(visit)
 }
 
-func (g *Gossiper) visitPeerByState(visit func(Peer) bool, states ...int) {
+func (g *Gossiper) visitPeerByState(visit func(MembershipPeer) bool, states ...int) {
 	for _, peers := range g.byRegion {
 		for _, peer := range peers {
 			state, _, _ := peer.State()
@@ -243,8 +327,12 @@ func (g *Gossiper) visitPeerByState(visit func(Peer) bool, states ...int) {
 	}
 }
 
-func (g *Gossiper) VisitPeerByState(visit func(Peer) bool, states ...int) {
+func (g *Gossiper) VisitPeerByState(visit func(MembershipPeer) bool, states ...int) {
 	g.lock.RLock()
 	defer g.lock.RUnlock()
 	g.visitPeerByState(visit)
 }
+
+func (g *Gossiper) OnRemove(callback func(MembershipPeer)) { g.onRemove = callback }
+
+func (g *Gossiper) OnAppend(callback func(MembershipPeer)) { g.onAppend = callback }
